5b4209147cc61ba4b83d9e3d8f05c0d9948ab195
learningreac <learningreact2020@gmail.com>
2021-05-16 11:47:45 -0800

    useEffect array depedency problem

--- src/BingmapAPP.js
+++ src/BingmapAPP.js
@@ -8,6 +8,8 @@ import './style.css';
 import {bmapaddpushpins} from './maputil'
 
 
+const center = [47.60357, -122.32945];
+
 /*
 * 0 fetch data 
 * 1 pan map will add more playgournds
@@ -20,16 +22,15 @@ export default function App() {
   const [bmap, setMap]= useState(null);
 
   const credentials = "AksOASfzdybmndjlOxhWnhZaNtzG5CMgqUFIgB5Ji8W6Gr748WQL5mijk5w4OmDD";
-  const center = [47.60357, -122.32945];
   
-
   useEffect(()=>{
+    console.log(ppdata);
     fetch(`https://dev.virtualearth.net/REST/v1/LocalSearch/?query=playground&userLocation=${center[0]},${center[1]}&maxResults=20&key=${credentials}`)
     .then(response => response.json())
     .then(obj => obj.resourceSets[0].resources)
     .then(setData)
     .catch(console.error);
-  }, [center]);
+  }, [center]); //  deep depedency problem
 
   useEffect(()=>{
     if (null!=bmap&&undefined!=ppdata) {
