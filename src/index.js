452e6c8e54335448dee756f51bd8369bb6097856
learningreac <learningreact2020@gmail.com>
2021-05-20 21:19:32 -0800

    copy the current folder for re-factoring movieapp 2.0, using useContext

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import App from './App';
 //import BingmapAPP from './BingmapAPP';
-//import MovieAPP from './movieapp/MovieAPP'
-import MovieAPP2 from './movieapp/MovieAPP2'
+import MovieAPP from './movieapp2.0/MovieAPP'
+//import MovieAPP2 from './movieapp/MovieAPP2'
 
 ReactDOM.render(
   <React.StrictMode>
-    <MovieAPP2 />
+    <MovieAPP />
   </React.StrictMode>,
   document.getElementById('root')
 );
