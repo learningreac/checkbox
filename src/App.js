0aa49d8ec56d980ebbce1533b1a7ce7223010642
learningreac <learningreact2020@gmail.com>
2021-05-25 10:03:43 -0800

    Add a home page for displaying all projects, click on home page item can routes to seperate projects.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import { useRoutes } from "react-router-dom";
 import GithubUserAPP from './GithubUser/GithubUser';
 import BingMapApp from './BingMapAPP/BingmapAPP';
+import RouterApp from './BasicRouter/RouterAPP';
+import Home from './Home';
 
 
 function App() {
   let element = useRoutes([
-    {path:'/', element:<GithubUserAPP/>,},
+    {path:'/', element:<Home />,},
     { path: "Githubuser", element: <GithubUserAPP />, },
     { path: "Bingmap", element: <BingMapApp />, },
+    { path: "Basicrouter", element: <RouterApp />, },
   ]);
   return element;
 }
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from 'react-router-dom'; 
+
+export default function Home() {
+    return (
+        <div>
+            <h1>[Project Website]</h1>
+            <nav>
+                <Link  className='action-item' to='Githubuser'>GitHubUser</Link>
+                <Link  className='action-item' to='Bingmap'>BingMapApp</Link>
+                <Link  className='action-item' to='Basicrouter'>BasicRouter</Link>
+            </nav>
+        </div>
+    );
+}
